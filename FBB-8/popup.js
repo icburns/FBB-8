c6c25c732626fcd5bee02db8945d3729161a9bf1
icburns <ebeptonian@gmail.com>
2016-03-04 00:41:29 -0800

    v0.1.4
    Added production script src and animations to crawl

--- FBB-8/popup.js
+++ FBB-8/popup.js
@@ -5,22 +5,46 @@ window.onload = function(){
     var isChangelogOpen = false;
     debugger;
     document.getElementById('crawl-launcher').onclick = toggleChangelogCrawl;
+    document.getElementById('changelog-panel').addEventListener("animationend", resetAnimationIterations, false);
+//    document.getElementById('changelog-panel').addEventListener("animationiteration", resetAnimationIterations, false);
 
     function toggleChangelogCrawl(e) {
+        var crawlLauncher = document.getElementById('crawl-launcher');
         var crawltext = document.getElementById('crawltext');
         var changelog = document.getElementById('changelog');
         var changelogPanel = document.getElementById('changelog-panel');
 
         if(!isChangelogOpen){
-            changelog.classList.add('show');
+            changelog.classList.add('active');
             crawltext.classList.add('active');
-            changelogPanel.classList.add('active');
+            changelogPanel.classList.remove('inactive');
+            changelogPanel.classList.remove('active');
+            setTimeout(
+                function(){
+                    changelogPanel.classList.add('active');
+                },.1
+            );
+            crawlLauncher.innerHTML = 'Hide Changelog';
             isChangelogOpen = true;
+
         }else{
-            changelog.classList.remove('show');
+            changelog.classList.remove('active');
             crawltext.classList.remove('active');
             changelogPanel.classList.remove('active');
+            changelogPanel.classList.remove('inactive');
+            setTimeout(
+                function(){
+                    changelogPanel.classList.add('inactive');
+                },.1
+            );            crawlLauncher.innerHTML = 'View Changelog';
             isChangelogOpen = false;
         }
+    }
+
+
+    function resetAnimationIterations(e) {
+        console.log(e);
+        console.log(document.getElementById('changelog-panel'));
+        e.preventDefault();
     }
 }();
